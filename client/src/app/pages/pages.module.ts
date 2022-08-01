1acd8160e4afceae20e4ece0245db9cc5af5e16e
Julius Markauskas <juliusm9791@gmail.com>
2022-08-01 17:15:54 -0400

    added post card and sigle post component

--- client/src/app/pages/pages.module.ts
+++ client/src/app/pages/pages.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './posts/post/post.component';
 
 @NgModule({
-  declarations: [HomeComponent, PostsComponent],
+  declarations: [HomeComponent, PostsComponent, PostComponent],
   imports: [CommonModule, SharedModule],
   exports: [HomeComponent, PostsComponent],
 })
