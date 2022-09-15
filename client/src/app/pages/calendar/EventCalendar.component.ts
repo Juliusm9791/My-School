a1d09086f311f170c399f61ab909729d3965c34f
Julius Markauskas <juliusm9791@gmail.com>
2022-09-15 00:41:10 -0400

    added event from post to calendar

--- client/src/app/pages/calendar/EventCalendar.component.ts
+++ client/src/app/pages/calendar/EventCalendar.component.ts
@@ -1,52 +1,25 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { CalendarEvent, CalendarView } from 'angular-calendar';
-import { isSameDay, isSameMonth } from 'date-fns';
+// import { isSameDay, isSameMonth } from 'date-fns';
+import { PostsService } from '../posts/posts.service';
 
-
-const colors = {
-  red: {
-    primary: '#ad2121',
-    secondary: '#FAE3E3',
-  },
-  blue: {
-    primary: '#1e90ff',
-    secondary: '#D1E8FF',
-  },
-  yellow: {
-    primary: '#e3bc08',
-    secondary: '#FDF1BA',
-  },
-};
 
 @Component({
   selector: 'app-eventCalendar',
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './EventCalendar.component.html',
   styleUrls: ['./EventCalendar.component.css'],
-
-  // template: '<p>Test</p>',
 })
-export class EventCalendarComponent {
+
+export class EventCalendarComponent implements OnInit {
   view: CalendarView = CalendarView.Month;
+  events: CalendarEvent<{ id: string }>[]  = [];
 
   viewDate: Date = new Date();
+  constructor(private postsService: PostsService) {
+    this.events = this.postsService.filterEvents();
+  }
 
-  events: CalendarEvent<{ id: number }>[] = [
-    {
-      title: 'Event 1',
-      color: colors.yellow,
-      start: new Date(),
-      meta: {
-        id: 1,
-      },
-    },
-    {
-      title: 'Event 2',
-      color: colors.blue,
-      start: new Date(),
-      meta: {
-        id: 2,
-      },
-    },
-  ];
+  ngOnInit(): void {}
+
 }
