a741557d343f57007dbcb81b9b9ecaa7979c4284
Olsen <chrisolsen1993@gmail.com>
2022-09-21 00:37:58 -0400

    add post to the dataBase

--- client/src/app/pages/account/profile/form-post/form-post.component.ts
+++ client/src/app/pages/account/profile/form-post/form-post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormPostService } from './form-post.service';
 
 @Component({
   selector: 'app-form-post',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-post.component.css']
 })
 export class FormPostComponent implements OnInit {
-
-  constructor() { }
+  postForm = new FormGroup({
+    postTitle: new FormControl('', [Validators.required]),
+    postDescription: new FormControl('', [Validators.required]),
+  });
+  constructor( private postFormService:FormPostService ) { }
 
   ngOnInit(): void {
   }
-
+  onSubmit() {
+    const title: any= this.postForm.controls.postTitle.value
+    const description: any=this.postForm.controls.postDescription.value
+   console.log(title)
+   console.log(description)
+this.postFormService.addPost(title, description)
+    // this.loginSignupService.userLoginSignup(SIGNUP, {
+    //   firstName: this.signUpForm.controls.firstName.value,
+    //   middleName: this.signUpForm.controls.middleName.value,
+    //   lastName: this.signUpForm.controls.lastName.value,
+    //   email: this.signUpForm.controls.email.value,
+    //   password: this.signUpForm.controls.password.value,
+    // });
+  }
 }
--- client/src/app/pages/account/profile/form-post/form-post.service.ts
+++ client/src/app/pages/account/profile/form-post/form-post.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { ADD_POST } from 'src/app/services/graphql/mutations';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormPostService {
+  loading: boolean = true;
 
-  constructor() { }
+  constructor(private apollo: Apollo,) { }
+  addPost(title: string, description: string) {
+    this.apollo
+      .mutate({
+        mutation: ADD_POST,
+        variables: { title: title, description: description },
+      })
+      .subscribe(
+        (result: any) => {
+          console.log('got data', result);
+          this.loading = result.loading;
+          // this.changeLoading.emit(this.loading);
+          // console.log(this.loading, 'login service');
+          // let userData: any;
+          console.log(result)
+          // this.post = userData.user;
+          // this.authService.login(
+          //   mutationDefinition === 'addUser'
+          //     ? result.data.addUser.token
+          //     : mutationDefinition === 'login'
+          //     ? result.data.login.token
+          //     : new Error('Something went wrong during login!')
+          // );
+          // this.changeMe.emit(this.me);
+          // !this.loading && this.router.navigate(['/account/profile']);
+        },
+        (error) => {
+          console.log('add post error', error);
+        }
+      );
+
+
+  }
 }
