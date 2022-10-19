daee7ccc56ad387de67b6293214a4e95ce7f3854
David Dyer <davidrossdyer@gmail.com>
2022-10-18 23:34:12 -0400

    create post form started / nav is now scaling

--- src/app/pages/account/profile/form-post/form-post.component.ts
+++ src/app/pages/account/profile/form-post/form-post.component.ts
@@ -31,3 +31,14 @@ this.postFormService.addPost(title, description)
     // });
   }
 }
+
+
+
+// import {Component} from '@angular/core';
+
+// /** @title Basic datepicker */
+// @Component({
+//   selector: 'datepicker-overview-example',
+//   templateUrl: 'datepicker-overview-example.html',
+// })
+// export class DatepickerOverviewExample {}
