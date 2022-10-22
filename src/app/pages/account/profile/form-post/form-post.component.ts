e7e0357f756f10f15dcf39030b171be2a634fd8f
JuliusM <juliusm9791@gmail.com>
2022-10-22 16:06:59 -0400

    word input validation

--- src/app/pages/account/profile/form-post/form-post.component.ts
+++ src/app/pages/account/profile/form-post/form-post.component.ts
@@ -4,6 +4,8 @@ import { DepartmentsService } from 'src/app/pages/departments/departments.servic
 import { Department } from 'src/app/types/types';
 import { FormPostService } from './form-post.service';
 
+const badWords: string[] = ['foo', 'boo'];
+
 @Component({
   selector: 'app-form-post',
   templateUrl: './form-post.component.html',
@@ -11,14 +13,20 @@ import { FormPostService } from './form-post.service';
 })
 export class FormPostComponent implements OnInit {
   departmentList: Department[] = [];
+
   postForm = new FormGroup({
-    postTitle: new FormControl('', [Validators.required]),
-    postDescription: new FormControl('', [Validators.required]),
+    postTitle: new FormControl('', [
+      Validators.required,
+      this.restrictedWords(badWords),
+    ]),
+    postDescription: new FormControl('', [
+      Validators.required,
+      this.restrictedWords(badWords),
+    ]),
     isEvent: new FormControl(false),
     eventDate: new FormControl(null),
     departmentId: new FormControl(null),
   });
-
 
   constructor(
     private postFormService: FormPostService,
@@ -32,34 +40,33 @@ export class FormPostComponent implements OnInit {
   }
 
   onSubmit() {
-    const title: any = this.postForm.controls.postTitle.value;
-    const description: any = this.postForm.controls.postDescription.value;
-    const isPostEvent: any = this.postForm.controls.isEvent.value;
-    const selectedDepartmentId: any = this.postForm.controls.departmentId.value;
-    const eventDate: any = this.postForm.controls.eventDate.value;
-    this.postFormService.addPost(
-      title,
-      description,
-      isPostEvent,
-      selectedDepartmentId,
-      eventDate
-    );
+    if (this.postForm.valid) {
+      const title: any = this.postForm.controls.postTitle.value;
+      const description: any = this.postForm.controls.postDescription.value;
+      const isPostEvent: any = this.postForm.controls.isEvent.value;
+      const selectedDepartmentId: any =
+        this.postForm.controls.departmentId.value;
+      const eventDate: any = this.postForm.controls.eventDate.value;
+      this.postFormService.addPost(
+        title,
+        description,
+        isPostEvent,
+        selectedDepartmentId,
+        eventDate
+      );
+    }
+  }
+  restrictedWords(words: string[]) {
+    return (control: FormControl): { [key: string]: any } | null => {
+      if (!words) return null;
 
-    // this.loginSignupService.userLoginSignup(SIGNUP, {
-    //   firstName: this.signUpForm.controls.firstName.value,
-    //   middleName: this.signUpForm.controls.middleName.value,
-    //   lastName: this.signUpForm.controls.lastName.value,
-    //   email: this.signUpForm.controls.email.value,
-    //   password: this.signUpForm.controls.password.value,
-    // });
+      let invalidWords = words
+        .map((w) => (control.value.includes(w) ? w : null))
+        .filter((w) => w != null);
+
+      return invalidWords && invalidWords.length > 0
+        ? { restrictedWords: invalidWords.join(', ') }
+        : null;
+    };
   }
 }
-
-// import {Component} from '@angular/core';
-
-// /** @title Basic datepicker */
-// @Component({
-//   selector: 'datepicker-overview-example',
-//   templateUrl: 'datepicker-overview-example.html',
-// })
-// export class DatepickerOverviewExample {}
