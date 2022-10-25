07971bdffc360d5f5c0b7b6085855bf15dffe5f8
JuliusM <juliusm9791@gmail.com>
2022-10-25 01:03:41 -0400

    update post form fix

--- src/app/pages/account/profile/form-post/form-post.component.ts
+++ src/app/pages/account/profile/form-post/form-post.component.ts
@@ -1,9 +1,8 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { Router } from 'express';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DepartmentsService } from 'src/app/pages/departments/departments.service';
-import { PostDetailsService } from 'src/app/pages/posts/post-details/post-details.service';
 import { PostsService } from 'src/app/pages/posts/posts.service';
 import { Department, Post } from 'src/app/types/types';
 import { FormPostService } from './form-post.service';
@@ -28,51 +27,64 @@ export class FormPostComponent implements OnInit {
       this.restrictedWords(badWords),
     ]),
     isEvent: new FormControl(false),
-    eventDate: new FormControl(null),
-    departmentId: new FormControl(null),
+    eventDate: new FormControl(''),
+    departmentId: new FormControl(''),
   });
-  post: Post = {} as Post
-  postId: any
+
+  post: Post = {} as Post;
+  postId: any = this.route.snapshot.paramMap.get('id');
+  minDate: any = formatDate(new Date(), 'yyyy-MM-ddTHH:mm', 'en');
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private postFormService: FormPostService,
     private departmentsService: DepartmentsService,
     private postsService: PostsService
-
   ) {
+    this.postForm.controls.eventDate.setValue(
+      formatDate(new Date(), 'yyyy-MM-ddTHH:mm', 'en')
+    );
+    this.postForm.controls.eventDate.setValue(
+      formatDate(new Date(), 'yyyy-MM-ddTHH:mm', 'en')
+    );
+    if (this.postId) {
+      this.post = this.postsService.singlePost(this.postId);
 
-    // this.post = this.postsService.singlePost(this.postId)
-
-    //   this.postForm.controls.postTitle.setValue(post.title)
-    //   this.postForm.controls.postDescription.setValue(post.description)
-
-
+      this.postForm.controls.postTitle.setValue(this.post.title);
+      this.postForm.controls.postDescription.setValue(this.post.description);
+      this.postForm.controls.isEvent.setValue(this.post.isEvent);
+      this.postForm.controls.departmentId.setValue(this.post.departmentId._id);
+      this.postForm.controls.eventDate.setValue(
+        formatDate(this.post.eventDate, 'yyyy-MM-ddTHH:mm', 'en')
+      );
+    }
   }
 
   ngOnInit() {
-    this.postId = this.route.snapshot.paramMap.get('id');
-    // console.log(this.postService)
     if (this.departmentsService.departments.length === 0)
       this.departmentsService.queryDepartment();
     this.departmentList = this.departmentsService.departments;
   }
 
   onSubmit() {
-    if (this.postForm.valid) {
-      const title: any = this.postForm.controls.postTitle.value;
-      const description: any = this.postForm.controls.postDescription.value;
-      const isPostEvent: any = this.postForm.controls.isEvent.value;
-      const selectedDepartmentId: any =
-        this.postForm.controls.departmentId.value;
-      const eventDate: any = this.postForm.controls.eventDate.value;
-      this.postFormService.addPost(
-        title,
-        description,
-        isPostEvent,
-        selectedDepartmentId,
-        eventDate
-      );
+    if (!this.postId) {
+      if (this.postForm.valid) {
+        const title: any = this.postForm.controls.postTitle.value;
+        const description: any = this.postForm.controls.postDescription.value;
+        const isPostEvent: any = this.postForm.controls.isEvent.value;
+        const selectedDepartmentId: any =
+          this.postForm.controls.departmentId.value;
+        const eventDate: any = this.postForm.controls.eventDate.value;
+        this.postFormService.addPost(
+          title,
+          description,
+          isPostEvent,
+          selectedDepartmentId,
+          eventDate
+        );
+      }
+    } else {
+      console.log(this.postForm);
     }
   }
   restrictedWords(words: string[]) {
@@ -87,5 +99,8 @@ export class FormPostComponent implements OnInit {
         ? { restrictedWords: invalidWords.join(', ') }
         : null;
     };
+  }
+  handleCancel() {
+    this.router.navigate(['/account/profile/']);
   }
 }
