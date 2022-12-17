beb0c9a06fdc9be858d28df7811692996f8d2c92
David Dyer <davidrossdyer@gmail.com>
2022-12-17 10:54:25 -0500

    added profile front component

---
+++ src/app/pages/account/profile/profile-front/profile-front.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-front',
+  templateUrl: './profile-front.component.html',
+  styleUrls: ['./profile-front.component.css']
+})
+export class ProfileFrontComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
