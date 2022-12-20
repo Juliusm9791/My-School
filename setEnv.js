8729fdc770b113737b45f12f31a42e2dbcec238a
Julius M <juliusm9791@gmail.com>
2022-12-20 08:25:58 -0500

    env updates

--- setEnv.js
+++ setEnv.js
@@ -1,37 +1,37 @@
 const setEnv = () => {
-  const fs = require('fs');
+  const fs = require("fs");
   const writeFile = fs.writeFile;
   // Configure Angular `environment.ts` file path
-  const targetPath = './src/environments/environment.ts';
-  const targetPathProd = './src/environments/environment.prod.ts';
+  const targetPath = "./src/environments/environment.ts";
+  const targetPathProd = "./src/environments/environment.prod.ts";
   // Load node modules
   // const colors = require("colors");
-  const appVersion = require('./package.json').version;
-  require('dotenv').config({
-    path: '.env',
+  const appVersion = require("./package.json").version;
+  require("dotenv").config({
+    path: ".env",
   });
   // `environment.ts` file structure
   const envConfigFile = `export const environment = {
-        ACCESS: '${process.env['ACCESS']}',
-        SECRET: '${process.env['SECRET']}',
-        BUCKET: '${process.env['BUCKET']}',
+        ACCESS: '${process.env["ACCESS"]}',
+        SECRET: '${process.env["SECRET"]}',
+        BUCKET: '${process.env["BUCKET"]}',
     production: false,
   };
   `;
   const envConfigFileProd = `export const environment = {
-    ACCESS: '${process.env['ACCESS']}',
-    SECRET: '${process.env['SECRET']}',
-    BUCKET: '${process.env['BUCKET']}',
+    ACCESS: '${process.env["ACCESS"]}',
+    SECRET: '${process.env["SECRET"]}',
+    BUCKET: '${process.env["BUCKET"]}',
   production: true,
   };
   `;
 
   console.log(
     // colors.magenta(
-    'The file `environment.ts` will be written with the following content: \n'
+    "The file `environment.ts` will be written with the following content: \n"
     // )
   );
-  writeFile(targetPath, envConfigFile, (err: any) => {
+  writeFile(targetPath, envConfigFile, (err) => {
     if (err) {
       console.error(err);
       throw err;
@@ -43,7 +43,7 @@ const setEnv = () => {
       );
     }
   });
-  writeFile(targetPathProd, envConfigFileProd, (err: any) => {
+  writeFile(targetPathProd, envConfigFileProd, (err) => {
     if (err) {
       console.error(err);
       throw err;
