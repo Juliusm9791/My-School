0816d8113cc2f9590255a09812def6d36d76b12d
Julius M <juliusm9791@gmail.com>
2022-12-21 19:02:13 -0500

    member data for profile front

--- src/app/pages/account/profile/profile-front/profile-front.component.ts
+++ src/app/pages/account/profile/profile-front/profile-front.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FacultyService } from 'src/app/pages/faculty/faculty.service';
+import { Me } from 'src/app/types/types';
 
 @Component({
   selector: 'app-profile-front',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-front.component.css']
 })
 export class ProfileFrontComponent implements OnInit {
+  facultyMemberID: any;
+  facultyMember : Me = {} as Me;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private facultyService: FacultyService) { }
 
   ngOnInit(): void {
+    this.facultyMemberID = this.route.snapshot.paramMap.get('id');
+    this.facultyMember = this.facultyService.singleFacultyMember(this.facultyMemberID)
+    console.log(this.facultyMember)
   }
+
 
 }
