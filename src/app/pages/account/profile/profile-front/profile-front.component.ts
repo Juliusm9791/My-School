86c6b73168e6726ddf110c9289de27071df97181
NewChap2022 <irisyangshu@gmail.com>
2023-01-10 21:58:20 -0500

    change profile-front component

--- src/app/pages/account/profile/profile-front/profile-front.component.ts
+++ src/app/pages/account/profile/profile-front/profile-front.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { FacultyService } from 'src/app/pages/faculty/faculty.service';
+import { ActivatedRoute, Router } from '@angular/router';
+// import { FacultyService } from 'src/app/pages/faculty/faculty.service';
+import { UserService } from 'src/app/services/service';
+import { LoginSignupService } from '../../login-signup.service';
 import { Me } from 'src/app/types/types';
 
 @Component({
@@ -9,20 +11,44 @@ import { Me } from 'src/app/types/types';
   styleUrls: ['./profile-front.component.css'],
 })
 export class ProfileFrontComponent implements OnInit {
-  facultyMemberID: any;
-  facultyMember: Me = {} as Me;
+  // facultyMemberID: any;
+  userId: any;
+  // facultyMember: Me = {} as Me;
+  user: any;
+  loading: boolean = true;
   defaultAvatar: string = '../../../../assets/images/account.png';
 
   constructor(
     private route: ActivatedRoute,
-    private facultyService: FacultyService
-  ) {}
+    private userService: UserService,
+    private loginSignupService: LoginSignupService,
+    private router: Router
+  ) {
+    this.userService.changeUser.subscribe((user) => {
+      this.user = user;
+    })
+    this.userService.changeUserLoading.subscribe((loading) => {
+      this.loading = loading;
+    })
+  }
 
   ngOnInit(): void {
-    this.facultyMemberID = this.route.snapshot.paramMap.get('id');
-    this.facultyMember = this.facultyService.singleFacultyMember(
-      this.facultyMemberID
-    );
-    console.log(this.facultyMember);
+    // this.facultyMemberID = this.route.snapshot.paramMap.get('id');
+    // this.facultyMember = this.facultyService.singleFacultyMember(
+    //   this.facultyMemberID
+    // );
+    this.userId = this.route.snapshot.paramMap.get('id');
+    if (this.userId === this.loginSignupService.me._id) {
+      this.router.navigate(['/account/profile']);
+      return
+    }; 
+    this.userService.queryUser(this.userId);
+    // console.log(this.facultyMember);
+  }
+
+  gatherData(element: any[], elementName: string) {
+    let result: string [] = []
+    element.forEach((e: any)=> result.push(e[elementName]));
+    return result.join(', ');
   }
 }
