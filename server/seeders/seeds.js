9320fd514f74599cbd1d7b690141c794402f8cdb
NewChap2022 <irisyangshu@gmail.com>
2023-01-31 22:24:23 -0500

    update seeds file

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -179,17 +179,21 @@ db.once("open", async () => {
   const posts = await Post.insertMany([
     {
       isVisible: true,
-      // isEvent: true,
-      // eventDate: "04-26-2023",
-      // eventEndDate: "04-26-2023",
-      // eventLocation: "Community Center",
+      isEvent: true,
+      eventDate: "04-26-2023",
+      eventEndDate: "04-26-2023",
+      eventLocation: "Community Center",
       title: "English Extra Credit Opportunities",
       departmentId: departments[1].id,
       gradeId: [grades[1]._id, grades[2]._id],
       description:
         "Each semester I have special opportunity to choose one of you to represent our class at the Speakers’ Showcase. All communications professors can nominate one student to submit their best speech. If you are nominated you will receive extra credit. In addition, a panel of judges will choose the top six presentations to be in the Showcase. If you are chosen to be in the Showcase, you will receive additional extra credit and possibly money or prizes. This is a win-win! The next showcase is April 26th. Your presentation videos will be due April 17th.",
-      // pictures: postPictures["https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/0.jpg
-      // "],
+      pictures: [
+        {
+          order: 0,
+          location: "https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/0.jpg"
+        }
+      ],
       userId: users[Math.floor(Math.random() * users.length)]._id,
       commentId: comments[Math.floor(Math.random() * comments.length)]._id,
       reactionId: reactions[Math.floor(Math.random() * reactions.length)]._id,
@@ -197,50 +201,101 @@ db.once("open", async () => {
 
     {
       isVisible: true,
+      isEvent: false,
       title: "Bus stop chaperone",
       departmentId: departments[13].id,
       gradeId: applyAllGrades,
       description:
         "Bus Riders: We are in search of an adult bus stop chaperone for each stop along the route. Please lookout for a signup sheet that will be circulating by our transportation officials. We appreciate any volunteers that want to step up to ensure safety of our students at each and every bus stop in the district.",
+      pictures: [
+        {
+          order: 0,
+          location: "https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/1.1.jpg"
+        },
+        {
+          order: 1,
+          location: "https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/1.jpg"
+        }
+      ],
       userId: users[Math.floor(Math.random() * users.length)]._id,
       commentId: comments[Math.floor(Math.random() * comments.length)]._id,
       reactionId: reactions[Math.floor(Math.random() * reactions.length)]._id,
     },
     {
       isVisible: true,
+      isEvent: true,
+      eventDate: "05-12-2023 9:00 AM",
+      eventEndDate: "05-12-2023 10:00 AM",
+      eventLocation: "Main Office",
       title: "Device Distribution",
       departmentId: departments[0].id,
       gradeId: [grades[2]._id, grades[3]._id, grades[4]._id, grades[5]._id],
       description:
         "Pitt County Schools is pleased to announce that they have transitioned to being a 1-to-1 district. Our second and third grade students will be receiving iPads while fourth and fifth graders will be receiving Chromebooks. Your child will use the same device for the remainder of the year. A detailed device agreement is coming home with the students and can be submitted when picking up devices, but before the device can be taken home. ",
+      pictures: [
+        {
+          order: 0,
+          location: "https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/2.1.jpg"
+        },
+        {
+          order: 1,
+          location: "https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/2.jpg"
+        }
+      ],
       userId: users[Math.floor(Math.random() * users.length)]._id,
       commentId: comments[Math.floor(Math.random() * comments.length)]._id,
       reactionId: reactions[Math.floor(Math.random() * reactions.length)]._id,
     },
     {
       isVisible: true,
+      isEvent: true,
+      eventDate: "06-03-2023",
+      eventEndDate: "06-03-2023",
+      eventLocation: "Whole School",
       title: "Open House",
       departmentId: departments[0].id,
       gradeId: [grades[2]._id, grades[3]._id],
       description:
         "Open House will look different this year. Students who are coming on A and B weeks will participate in a virtual Open House on December 1st for 1st-5th grade.  Each teacher will send out a virtual open house video to view by 4:30pm. Your child’s teacher will contact you to either set up times to meet virtually through Zoom or do a live question and answer session for parents who would like to attend. We understand this may cause some anxiety, however, we can assure you we will be available to answer questions, address concerns, and introduce ourselves to our Eagles!",
+      pictures: [
+        {
+          order: 0,
+          location: "https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/3.jpg"
+        }
+      ],
       userId: users[Math.floor(Math.random() * users.length)]._id,
       commentId: comments[Math.floor(Math.random() * comments.length)]._id,
       reactionId: reactions[Math.floor(Math.random() * reactions.length)]._id,
     },
     {
       isVisible: true,
+      isEvent: false,
       title: "Masks and Hand washing",
       departmentId: departments[8].id,
       gradeId: applyAllGrades,
       description:
         "Students will be provided 5 masks and a lanyard. Students may choose to use these masks, or any other appropriate mask. Please do not send students with masks that contain inappropriate images or words. The lanyard is to be used to make sure that while students are eating, the masks do not fall to the ground or become mixed up with another student's mask.",
+      pictures: [
+        {
+          order: 0,
+          location: "https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/4.1.jpg"
+        },
+        {
+          order: 1,
+          location: "https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/4.2.jpg"
+        },
+        {
+          order: 2,
+          location: "https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/4.jpg"
+        }
+      ],
       userId: users[Math.floor(Math.random() * users.length)]._id,
       commentId: comments[Math.floor(Math.random() * comments.length)]._id,
       reactionId: reactions[Math.floor(Math.random() * reactions.length)]._id,
     },
     {
       isVisible: true,
+      isEvent: false,
       title: "School Lunch",
       departmentId: departments[14].id,
       gradeId: applyAllGrades,
@@ -252,44 +307,88 @@ db.once("open", async () => {
     },
     {
       isVisible: true,
+      isEvent: true,
+      eventDate: "06-20-2023 6:00 PM",
+      eventEndDate: "06-20-2023 9:00 PM",
+      eventLocation: "School Library",
       title: "PTA Meeting",
       departmentId: departments[0].id,
       gradeId: [grades[1]._id, grades[2]._id],
       description:
         "We would like to invite our first & second grader parents to our second PTA meeting of this academic year. It will take place in the school library at 6:45pm on Monday November 7th. We look forward to seeing you all and discussing next year's curriculum revisions in more detail",
+      pictures: [
+        {
+          order: 0,
+          location: "https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/6.png"
+        }
+      ],
       userId: users[Math.floor(Math.random() * users.length)]._id,
       commentId: comments[Math.floor(Math.random() * comments.length)]._id,
       reactionId: reactions[Math.floor(Math.random() * reactions.length)]._id,
     },
     {
       isVisible: true,
+      isEvent: true,
+      eventDate: "07-02-2023 7:00 PM",
+      eventEndDate: "07-02-2023 8:30 PM",
+      eventLocation: "Gymnasium",
       title: "Graduation Concert!",
       departmentId: departments[6].id,
       gradeId: [grades[5]._id],
       description:
         "This year our graduating fifth graders will enjoy a concert courtesy of our own school band, perform in the gymnasium on April 27th at 11:30am just before first period lunch. We will celebrate and also raise funds for Bully Prevention and towards aiding local animal shelters.",
+      pictures: [
+        {
+          order: 0,
+          location: "https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/7.1.jpg"
+        },
+        {
+          order: 1,
+          location: "https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/7.jpg"
+        }
+      ],
       userId: users[Math.floor(Math.random() * users.length)]._id,
       commentId: comments[Math.floor(Math.random() * comments.length)]._id,
       reactionId: reactions[Math.floor(Math.random() * reactions.length)]._id,
     },
     {
       isVisible: true,
+      isEvent: true,
+      eventDate: "10-02-2023",
+      eventEndDate: "10-03-2023",
+      eventLocation: "Room 101",
       title: "School Pictures",
       departmentId: departments[0].id,
       gradeId: [grades[1]._id, grades[2]._id],
       description:
         "School pictures are scheduled to take place on Tuesday, October 2nd & Wednesday October 3rd for the First and Second grade classes only. We will continue in about a week with the 3rd graders once they finish standardized testing at the end of the month. Remember to wear your best outfit and smile!",
+      pictures: [
+        {
+          order: 0,
+          location: "https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/8.jpg"
+        }
+      ],
       userId: users[Math.floor(Math.random() * users.length)]._id,
       commentId: comments[Math.floor(Math.random() * comments.length)]._id,
       reactionId: reactions[Math.floor(Math.random() * reactions.length)]._id,
     },
     {
       isVisible: true,
+      isEvent: true,
+      eventDate: "11-07-2023 10:00 AM",
+      eventEndDate: "11-07-2023 12:00 PM",
+      eventLocation: "Biology Classroom",
       title: "Biology Quiz",
       departmentId: departments[3].id,
       gradeId: [grades[4]._id],
       description:
         "Module Quiz: Heads up fourth graders, this week’s quiz includes 25 multiple-choice questions, and you have one hour complete the assignment once you begin. The quiz takes place on Friday.  Presentation: Be sure to reference the rubric for this assignment that can be found in the course documents folder. As always, I am happy to help!",
+      pictures: [
+        {
+          order: 0,
+          location: "https://user-images-8d8a6cbf-fd68-4ab3-bbdf-fe392c16d283.s3.us-east-2.amazonaws.com/9.jpg"
+        }
+      ],
       userId: users[Math.floor(Math.random() * users.length)]._id,
       commentId: comments[Math.floor(Math.random() * comments.length)]._id,
       reactionId: reactions[Math.floor(Math.random() * reactions.length)]._id,
