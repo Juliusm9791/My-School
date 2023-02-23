684b0ff554da58a8ea33e7d2dc9dedbf09529475
NewChap2022 <irisyangshu@gmail.com>
2023-02-22 22:02:05 -0500

    show faculty members on homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Department, Post } from 'src/app/types/types';
+import { Department, Me, Post } from 'src/app/types/types';
 import { DepartmentsService } from '../departments/departments.service';
 import { PostsService } from '../posts/posts.service';
+import { FacultyService } from '../faculty/faculty.service';
 
 @Component({
   selector: 'app-home',
@@ -14,16 +15,23 @@ export class HomeComponent implements OnInit {
   loading: boolean = true;
   error: any;
   departments: Department[] = [];
+  allFacultyMembers: Me[] = [];
   private _isUserPosts: boolean = false;
 
   constructor(
     private postsService: PostsService,
-    private departmentService: DepartmentsService
+    private departmentService: DepartmentsService,
+    private facultyService: FacultyService
   ) {
     this.departmentService.changeDepartments.subscribe((department) => {
         this.departments = department
         this.departments = this.departments.slice(0, 6)
     });
+
+    this.facultyService.changeFacultyMembers.subscribe((users) => {
+      this.allFacultyMembers = users;
+      this.allFacultyMembers = this.allFacultyMembers.slice(0, 6)
+  });
 
     this.postsService.changePosts.subscribe((posts) => {
       this.posts = posts;
@@ -43,6 +51,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.postsService.queryPosts();
     this.departmentService.queryDepartment();
+    this.facultyService.queryFacultyMembers();
   }
   likes(comment: any) {
     return this.postsService.countLikes(comment);
